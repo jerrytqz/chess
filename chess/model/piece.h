5b65ecfaa469ee88ec91d74a05e705fbd0f63562
jerrytqz <jerryzhengpro@gmail.com>
2024-07-24 22:35:01 -0400

    Fix some bugs in piece.h

--- chess/model/piece.h
+++ chess/model/piece.h
@@ -6,12 +6,8 @@
 #include "coordinate.h"
 
 class Piece {
-    Coordinate position;
-    PieceColour colour;
-    PieceType pieceType;
-    Board* board;
 public:
-    enum class PieceColour {
+    enum class Colour {
         White,
         Black
     };
@@ -25,14 +21,19 @@ public:
         Pawn
     };
 
-    Piece(Coordinate position, PieceColour colour, PieceType pieceType, Board* board) 
-        : position{position}, colour{colour}, pieceType{pieceType} board{board} {}
+    Piece(Coordinate position, Colour colour, PieceType pieceType, Board* board) 
+        : position{position}, colour{colour}, pieceType{pieceType}, board{board} {}
     virtual ~Piece() = default;
 
     virtual PieceType getPieceType() = 0;
     virtual std::vector<Coordinate> getValidMoves() = 0;
     virtual bool makeMove(Coordinate dest) = 0;
     virtual bool canTargetSquare(Coordinate square) = 0;
+private:
+    Coordinate position;
+    Colour colour;
+    PieceType pieceType;
+    Board* board;
 };
 
 #endif
