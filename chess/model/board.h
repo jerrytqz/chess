c7c29638def8e2a5e25998589715a43dd8fbc2df
Cheney Cai <cheneycai2004@hotmail.com>
2024-07-25 15:41:20 -0400

    board class header

--- chess/model/board.h
+++ chess/model/board.h
@@ -1,8 +1,38 @@
 #ifndef BOARD_H
 #define BOARD_H
 
+#include "piece.h"
+#include "coordinate.h"
+
 class Board {
-public:
+    public:
+        enum BoardState {
+            Default,
+            WhiteChecked,
+            BlackChecked,
+            WhiteCheckmated,
+            BlackCheckmated,
+            Stalemate
+        };
+
+        Board(int boardDimension); //CTOR
+        ~Board(); //DTOR
+
+        void computeBoardState();
+        BoardState getBoardState();
+        Piece getPiece();
+        int getBoardDimension();
+        bool takeTurn(Coordinate from, Coordinate to, Piece::Colour col);
+        bool promote(Coordinate pos, Piece::PieceType pieceType, Piece::Colour col);
+        bool addPiece(Coordinate pos, Piece* piece);
+        bool removePiece(Coordinate pos);
+
+    protected:
+
+    private:
+        Piece*** board;
+        int boardDimension;
+        BoardState boardState;
 };
 
 #endif
