4567697e6a5206289879ac18b63d6cf4be38bf76
Cheney Cai <cheneycai2004@hotmail.com>
2024-07-26 20:00:49 -0400

    initial Game class implementation

--- chess/controller/game.cc
+++ chess/controller/game.cc
@@ -1,7 +1,7 @@
 #include "game.h"
 
 Game::Game(Board* board, Player* whitePlayer, Player* blackPlayer): board{board}, whitePlayer{whitePlayer}, blackPlayer{blackPlayer} {
-
+    //no need for implementation
 }
 
 Game::~Game() {
@@ -9,3 +9,49 @@ Game::~Game() {
     delete whitePlayer; //REMEMBER TO DELETE PLAYERS DURING UPDATEPLAYER TOO
     delete blackPlayer;
 }
+
+// void Game::setUp() {
+
+// }
+
+// void Game::play() {
+
+// }
+
+void Game::updatePlayer(Colour colour, Player* player) {
+    if (colour == Colour::Black) {
+        blackPlayer = player;
+    }
+    else { //replace white player
+        whitePlayer = player;
+    }
+}
+
+void Game::detachObserver(Observer* obs) {
+    //find where observer is in list and delete it
+    for (auto it = observers.begin(); it != observers.end(); it++) {
+        if (*it == obs) {
+            observers.erase(it);
+            return;
+        }
+    }
+}
+
+void Game::attachObserver(Observer* obs) {
+    observers.push_back(obs); //not our fault if observer is included more than once
+}
+
+Game::GameState Game::getGameState() {
+    // return GameState {
+    //     whiteScore,
+    //     blackScore,
+    //     currentTurn,
+    //     board->getBoardDimension(),
+
+    //     board->getBoardState(),
+    // };
+}
+
+// void Game::notifyObservers() {
+
+// }
