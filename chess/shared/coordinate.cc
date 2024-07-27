503abc95dd4c60431581827e6304e916b8131d02
Cheney Cai <cheneycai2004@hotmail.com>
2024-07-27 18:39:11 -0400

    coordinate bugfix

--- chess/shared/coordinate.cc
+++ chess/shared/coordinate.cc
@@ -1,19 +1,20 @@
 #include "coordinate.h"
 
-Coordinate::Coordinate chessToCartesian(std::string chessCoordinate) {
+Coordinate::Coordinate Coordinate::chessToCartesian(std::string chessCoordinate) {
     int col = chessCoordinate[0] - 'a';
     int row = chessCoordinate[1] - 1;
-    return Coordinate::Coordinate{row, col};
+    return Coordinate{row, col};
 }
 
-std::string cartesianToChess(Coordinate::Coordinate cartesianCoordinate) {
+std::string Coordinate::cartesianToChess(Coordinate cartesianCoordinate) {
     std::string chessCoord = "";
     chessCoord.push_back(cartesianCoordinate.col + 'a');
     chessCoord.push_back(cartesianCoordinate.row + 1);
     return chessCoord;
 }
 
-bool checkValidChessCoord(std::string str) {
+bool Coordinate::checkValidChess(std::string possibleChessCoordinate) {
+    std::string str = possibleChessCoordinate;
     if (str.length() != 2 && str[0] >= 'a' && str[0] <= 'h' && str[1] >= '1' && str[1] <= '8')
         return true;
 
