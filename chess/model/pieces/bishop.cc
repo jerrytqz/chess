e2af373a4ccda016cb4e2f3f3f26be2814e7657c
Cheney Cai <cheneycai2004@hotmail.com>
2024-07-28 20:21:21 -0400

    bishop

--- chess/model/pieces/bishop.cc
+++ chess/model/pieces/bishop.cc
@@ -1,4 +1,5 @@
 #include "bishop.h"
+#include "../board.h"
 
 Bishop::Bishop(Coordinate::Coordinate position, Colour colour, Board* board):
     PieceClonable{position, colour, Piece::PieceType::Bishop, board} {}
@@ -6,13 +7,43 @@ Bishop::Bishop(Coordinate::Coordinate position, Colour colour, Board* board):
 std::vector<Coordinate::Coordinate> Bishop::getValidMoves() const {
     std::vector<Coordinate::Coordinate> validMoves;
 
-    //MISSING IMPLEMENTATION
+    std::vector<std::pair<int, int>> directions = {{
+        {1, 1},
+        {-1, 1},
+        {-1, -1},
+        {1, -1}
+    }};
+
+    for (const auto& [dRow, dCol] : directions) {
+        for (int i = 1;; ++i) {
+            Coordinate::Coordinate nextPos{position.row + i * dRow, position.col + i * dCol};
+
+            if (!Coordinate::checkBounds(nextPos, board->getBoardDimension())) {
+                break; //unable to move in this direction (edge of board)
+            }
+
+            if (board->getPiece(nextPos)) {
+                if (board->getPiece(nextPos)->getColour() != this->getColour()) {
+                    validMoves.push_back(nextPos); //capture piece
+                }
+                break; //stop moves in this direction
+            }
+
+            validMoves.push_back(nextPos); //empty square
+        }
+    }
 
     return validMoves;
 }
 
 bool Bishop::canTargetSquare(Coordinate::Coordinate square) const {
-    // Implementation goes here
+    std::vector<Coordinate::Coordinate> validMoves = getValidMoves();
+    if (std::find(validMoves.begin(), validMoves.end(), square) == validMoves.end()) { //square is not in valid moves
+        return false;
+    }
+    else {
+        return true;
+    }
 }
 
 bool Bishop::makeMove(Coordinate::Coordinate dest) {
