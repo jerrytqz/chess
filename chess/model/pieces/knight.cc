97590344527a24e26656f88ca358569853bc5064
Cheney Cai <cheneycai2004@hotmail.com>
2024-07-28 21:03:25 -0400

    knight

--- chess/model/pieces/knight.cc
+++ chess/model/pieces/knight.cc
@@ -1,4 +1,5 @@
 #include "knight.h"
+#include "../board.h"
 
 Knight::Knight(Coordinate::Coordinate position, Colour colour, Board* board):
     PieceClonable{position, colour, Piece::PieceType::Knight, board} {}
@@ -6,13 +7,41 @@ Knight::Knight(Coordinate::Coordinate position, Colour colour, Board* board):
 std::vector<Coordinate::Coordinate> Knight::getValidMoves() const {
     std::vector<Coordinate::Coordinate> validMoves;
 
-    //MISSING IMPLEMENTATION
+    std::vector<std::pair<int, int>> directions = {{
+        {1, 2},
+        {1, -2},
+        {-1, 2},
+        {-1, -2},
+        {2, 1},
+        {2, -1},
+        {-2, 1},
+        {-2, -1},
+    }};
+
+    for (const auto& [dRow, dCol] : directions) {
+        Coordinate::Coordinate nextPos{position.row + dRow, position.col + dCol};
+
+        if (!Coordinate::checkBounds(nextPos, board->getBoardDimension())) {
+            continue;
+        }
+
+        std::unique_ptr<Piece> destinationPiece = board->getPiece(nextPos);
+        if (destinationPiece == nullptr || destinationPiece->getColour() != this->getColour()) {
+            validMoves.push_back(nextPos);
+        }
+    }
 
     return validMoves;
 }
 
 bool Knight::canTargetSquare(Coordinate::Coordinate square) const {
-    // Implementation goes here
+    std::vector<Coordinate::Coordinate> validMoves = getValidMoves();
+    if (std::find(validMoves.begin(), validMoves.end(), square) == validMoves.end()) { //square is not in valid moves
+        return false;
+    }
+    else {
+        return true;
+    }
 }
 
 bool Knight::makeMove(Coordinate::Coordinate dest) {
