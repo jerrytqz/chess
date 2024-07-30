fea8dc09d27c5bc524ce79a803bb9284db2ed376
lctian <lctian@uwaterloo.ca>
2024-07-29 21:34:31 -0400

    row col labels for text observer

--- chess/view/textObserver.cc
+++ chess/view/textObserver.cc
@@ -6,6 +6,7 @@ void TextObserver::notify() { //this method interfaces with std::cout
 
     //print board
     for (int i = 0; i < gameState.boardDimension; i++) {
+        std::cout << 8 - i << " ";
         for (int j = 0; j < gameState.boardDimension; j++) {
             int row = gameState.boardDimension - 1 - i;
             if (gameState.board[row][j] != nullptr) { //piece exists
@@ -23,10 +24,17 @@ void TextObserver::notify() { //this method interfaces with std::cout
                 }
 
                 if (j == gameState.boardDimension - 1) { //last column in row
-                    std::cout << '\n';
+                    std::cout << std::endl;
                 }
         }
     }
+
+    std::cout << std::endl;
+    std::cout << "  ";
+    for (int i = 0; i < gameState.boardDimension; i++) {
+        std::cout << static_cast<char>('a' + i);
+    }
+    std::cout << std::endl;
 
     if (gameState.boardState == Board::BoardState::WhiteChecked) {
         std::cout << "White is in check.";
